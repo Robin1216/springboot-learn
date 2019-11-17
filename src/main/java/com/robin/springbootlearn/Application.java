2496b34a6e46306efa3b30c749ce624e928d25bd
robin1216 <bin.luo@silknets.com>
2019-11-17 23:33:13 +0800

    logback 学习、配置

--- src/main/java/com/robin/springbootlearn/Application.java
+++ src/main/java/com/robin/springbootlearn/Application.java
@@ -3,10 +3,8 @@ package com.robin.springbootlearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author robin
- */
-@SpringBootApplication
+
+@SpringBootApplication  // 表明是 spring boot 应用
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/robin/springbootlearn/app/controller/TestController.java
+++ src/main/java/com/robin/springbootlearn/app/controller/TestController.java
@@ -1,4 +1,34 @@
 package com.robin.springbootlearn.app.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
 public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @PostMapping("/first")
+    String index(){
+
+        log.trace("log.trace ....");
+        log.debug("log.debug ....");
+        log.info("log.info ....");
+        log.warn("log.warn ....");
+        log.error("log.error ....");
+
+        logger.trace("logger.trace ...");
+        logger.debug("logger.debug ...");
+        logger.info("logger.info ...");
+        logger.warn("logger.warn ...");
+        logger.error("logger.error ...");
+
+        return "the first app";
+    }
 }
